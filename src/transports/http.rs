6476859d207cbcfea6d7c461d1d4dbaf1117e7c7
root <root@localhost>
2020-06-06 20:34:14 +0700

    Implement HTTP digest authentication.

--- src/transports/http.rs
+++ src/transports/http.rs
@@ -6,12 +6,25 @@ use crate::errors::Result;
 use crate::transports::{BatchTransport, Transport};
 use crate::types::{Call, MethodCall, Params, Request, RequestId, Response, Version};
 
+#[derive(Clone)]
+pub struct DigestAuthCredentials {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Clone)]
+enum HttpCredentials {
+    DigestAuthCredentials(DigestAuthCredentials),
+    BearerAuthToken(String),
+    None,
+}
+
 /// HTTP transport
 #[derive(Clone)]
 pub struct HttpTransport {
     id: Arc<AtomicUsize>,
     url: String,
-    bearer_auth_token: Option<String>,
+    credentials: HttpCredentials,
     client: reqwest::Client,
 }
 
@@ -29,7 +42,7 @@ impl HttpTransport {
         Self {
             id: Default::default(),
             url: url.into(),
-            bearer_auth_token: None,
+            credentials: HttpCredentials::None,
             client: Self::new_client(),
         }
     }
@@ -39,19 +52,91 @@ impl HttpTransport {
         Self {
             id: Default::default(),
             url: url.into(),
-            bearer_auth_token: Some(token.into()),
+            credentials: HttpCredentials::BearerAuthToken(token.into()),
             client: Self::new_client(),
         }
     }
 
+    #[cfg(feature = "http-digest-auth")]
+    /// Create a new HTTP transport with given `url` and bearer `token`.
+    pub fn new_with_digest_auth<U: Into<String>, UN: Into<String>, P: Into<String>>(
+        url: U,
+        username: UN,
+        password: P
+    ) -> Self {
+        Self {
+            id: Default::default(),
+            url: url.into(),
+            credentials: HttpCredentials::DigestAuthCredentials(DigestAuthCredentials {
+                username: username.into(),
+                password: password.into(),
+            }),
+            client: Self::new_client(),
+        }
+    }
+
+    // Create a new HTTP transport with the given `url` and digest authentication credentials
+    // `DigestAuthCredentials`.
+
     async fn send_request(&self, request: &Request) -> Result<Response> {
-        let builder = self.client.post(&self.url).json(request);
-        let builder = if let Some(token) = &self.bearer_auth_token {
-            builder.bearer_auth(token)
-        } else {
-            builder
-        };
-        Ok(builder.send().await?.json().await?)
+        Ok(match &self.credentials {
+            #[cfg(feature = "http-digest-auth")]
+            HttpCredentials::DigestAuthCredentials(credentials) => {
+                let encoded_request = serde_json::to_vec(request).unwrap();
+
+                let auth_request_response = self.client.get(&self.url).send().await?;
+                let mut challenge = match auth_request_response.headers().get("WWW-Authenticate") {
+                    Some(challenge) => {
+                        digest_auth::parse(challenge.to_str().expect("invalid challenge"))
+                            .expect("invalid challenge")
+                    }
+
+                    None => panic!("server doesn't require digest authentication"),
+                };
+
+                let url_path = reqwest::Url::parse(&self.url).expect("bad url");
+                let url_path = url_path.path();
+
+                let auth_context = digest_auth::AuthContext::new_post(
+                    &credentials.username,
+                    &credentials.password,
+                    url_path,
+                    Some(&encoded_request)
+                );
+
+                let authorize_header = challenge.respond(&auth_context).unwrap().to_string();
+
+                self.client
+                    .post(&self.url)
+                    .header("Authorization", authorize_header)
+                    .body(encoded_request)
+                    .send()
+                    .await?
+                    .json()
+                    .await?
+            }
+
+            HttpCredentials::BearerAuthToken(token) => {
+                self.client
+                    .post(&self.url)
+                    .bearer_auth(token)
+                    .json(request)
+                    .send()
+                    .await?
+                    .json()
+                    .await?
+            }
+
+            HttpCredentials::None => {
+                self.client
+                    .post(&self.url)
+                    .json(request)
+                    .send()
+                    .await?
+                    .json()
+                    .await?
+            }
+        })
     }
 }
 
